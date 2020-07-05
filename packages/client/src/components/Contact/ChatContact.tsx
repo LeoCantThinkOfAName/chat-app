1b4f37039de532a1795789abfb9e5d01acdb79a1
leocantthinkofanamae <i7851@hotmail.com>
2020-07-05 14:21:05 +0800

    clear unused vars

--- packages/client/src/components/Contact/ChatContact.tsx
+++ packages/client/src/components/Contact/ChatContact.tsx
@@ -1,62 +1,62 @@
-import React from "react";
-import { ChatContact as ChatContactType } from "@chat-app/shared";
-import Badge from "@material-ui/core/Badge";
-import Typography from "@material-ui/core/Typography";
-import BaseContact from "./BaseContact";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import clsx from "clsx";
+import { ChatContact as ChatContactType } from '@chat-app/shared';
+import Badge from '@material-ui/core/Badge';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+import BaseContact from './BaseContact';
 
 interface Props {
-  contact: ChatContactType;
+	contact: ChatContactType;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    badge: {
-      alignItems: "center",
-      display: "flex",
-      position: "static",
-    },
-    circle: {
-      top: 0,
-      right: 0,
-      transform: "scale(1)",
-      transformOrigin: "50% 50%",
-      position: "static",
-      marginLeft: theme.spacing(1),
-    },
-  })
+	createStyles({
+		badge: {
+			alignItems: 'center',
+			display: 'flex',
+			position: 'static',
+		},
+		circle: {
+			top: 0,
+			right: 0,
+			transform: 'scale(1)',
+			transformOrigin: '50% 50%',
+			position: 'static',
+			marginLeft: theme.spacing(1),
+		},
+	})
 );
 
 const ChatContact: React.FC<Props> = ({ contact }) => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <BaseContact
-      contact={{
-        name: contact.userName,
-        thumbnail: null,
-        status: contact.userStatus,
-      }}
-      link={`/chat/${contact.chatRoomId}`}
-      showStatus
-    >
-      <Badge
-        overlap="circle"
-        badgeContent={contact.unread && contact.unread}
-        color="error"
-        classes={{
-          root: classes.badge,
-          anchorOriginTopRightCircle: classes.circle,
-        }}
-      >
-        <Typography component="p">{contact.userName}</Typography>
-      </Badge>
-      <Typography component="p" variant="caption" color="textSecondary">
-        Last message
-      </Typography>
-    </BaseContact>
-  );
+	return (
+		<BaseContact
+			contact={{
+				name: contact.userName,
+				thumbnail: null,
+				status: contact.userStatus,
+			}}
+			link={`/chat/${contact.chatRoomId}`}
+			showStatus
+		>
+			<Badge
+				overlap="circle"
+				badgeContent={contact.unread && contact.unread}
+				color="error"
+				classes={{
+					root: classes.badge,
+					anchorOriginTopRightCircle: classes.circle,
+				}}
+			>
+				<Typography component="p">{contact.userName}</Typography>
+			</Badge>
+			<Typography component="p" variant="caption" color="textSecondary">
+				Last message
+			</Typography>
+		</BaseContact>
+	);
 };
 
 export default ChatContact;
--- packages/client/src/pages/Chat.tsx
+++ packages/client/src/pages/Chat.tsx
@@ -1,8 +1,8 @@
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import InboxIcon from '@material-ui/icons/Inbox';
+import clsx from 'clsx';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
@@ -14,105 +14,97 @@ import { Message } from '../../../shared/dist/Message';
 import { fakeMessages } from '../assets/dev/fakeMessages';
 import { MyBubble } from '../components/ChatBubble';
 import OthersBubble from '../components/ChatBubble/OthersBubble';
-import { useGlobalStyles } from '../Theme';
-import clsx from 'clsx';
 import ChatInput from '../components/ChatInput';
+import { useGlobalStyles } from '../Theme';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    window: {
-      padding: theme.spacing(2),
-      "&>div": {
-        display: "flex",
-        flexDirection: "column",
-      },
-    },
-    paper: {
-      backgroundColor: theme.palette.background.paper,
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      margin: 0,
-      zIndex: 1,
-    },
-    box: {
-      height: theme.spacing(7),
-      display: "flex",
-      alignItems: "center",
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2),
-      width: "100%",
-      borderBottomWidth: "1px",
-      borderBottomColor: theme.palette.divider,
-      borderBottomStyle: "solid",
-    }
-  })
+	createStyles({
+		window: {
+			padding: theme.spacing(2),
+			'&>div': {
+				display: 'flex',
+				flexDirection: 'column',
+			},
+		},
+		paper: {
+			backgroundColor: theme.palette.background.paper,
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			width: '100%',
+			margin: 0,
+			zIndex: 1,
+		},
+		box: {
+			height: theme.spacing(7),
+			display: 'flex',
+			alignItems: 'center',
+			paddingLeft: theme.spacing(2),
+			paddingRight: theme.spacing(2),
+			width: '100%',
+			borderBottomWidth: '1px',
+			borderBottomColor: theme.palette.divider,
+			borderBottomStyle: 'solid',
+		},
+	})
 );
 
 const Conversation = ({ index, data }: ListChildComponentProps) => {
-  const context: Message = data[index];
-  return context.user.id === 1 ? (
-    <MyBubble message={context} />
-  ) : (
-    <OthersBubble message={context} />
-  );
+	const context: Message = data[index];
+	return context.user.id === 1 ? <MyBubble message={context} /> : <OthersBubble message={context} />;
 };
 
 const Chat = () => {
-  const { t } = useTranslation();
-  const { id } = useParams();
-  const classes = useStyles();
-  const globalClasses = useGlobalStyles();
-  const currentRoom = fakeMessages[id];
+	const { t } = useTranslation();
+	const { id } = useParams();
+	const classes = useStyles();
+	const globalClasses = useGlobalStyles();
+	const currentRoom = fakeMessages[id];
 
-  return (
-    <Box display="flex" flexDirection="column" flex={1}>
-      {id ? (
-        <Box display="flex" flexDirection="column" flex={1} position="relative">
-          <Box className={clsx(classes.paper, classes.box)}>
-            <Typography component="p" variant="subtitle2">User {id}</Typography>
-          </Box>
-          <Box className={classes.box} />
-          <Box flex={1}>
-            <AutoSizer>
-              {({ height, width }) => (
-                <DynamicSizeList
-                  height={height}
-                  width={width}
-                  itemCount={currentRoom.length}
-                  itemData={currentRoom}
-                  className={classes.window}
-                >
-                  {Conversation}
-                </DynamicSizeList>
-              )}
-            </AutoSizer>
-          </Box>
-          <ChatInput/>
-        </Box>
-      ) : (
-        <Box
-          flex={1}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            className={globalClasses.emptyPagePlaceholder}
-          >
-            <InboxIcon fontSize="large" />
-            <Typography component="p" variant="caption">
-              {t("general.placeholder.page.chat")}
-            </Typography>
-          </Box>
-        </Box>
-      )}
-    </Box>
-  );
+	return (
+		<Box display="flex" flexDirection="column" flex={1}>
+			{id ? (
+				<Box display="flex" flexDirection="column" flex={1} position="relative">
+					<Box className={clsx(classes.paper, classes.box)}>
+						<Typography component="p" variant="subtitle2">
+							User {id}
+						</Typography>
+					</Box>
+					<Box className={classes.box} />
+					<Box flex={1}>
+						<AutoSizer>
+							{({ height, width }) => (
+								<DynamicSizeList
+									height={height}
+									width={width}
+									itemCount={currentRoom.length}
+									itemData={currentRoom}
+									className={classes.window}
+								>
+									{Conversation}
+								</DynamicSizeList>
+							)}
+						</AutoSizer>
+					</Box>
+					<ChatInput />
+				</Box>
+			) : (
+				<Box flex={1} display="flex" alignItems="center" justifyContent="center">
+					<Box
+						display="flex"
+						flexDirection="column"
+						alignItems="center"
+						className={globalClasses.emptyPagePlaceholder}
+					>
+						<InboxIcon fontSize="large" />
+						<Typography component="p" variant="caption">
+							{t('general.placeholder.page.chat')}
+						</Typography>
+					</Box>
+				</Box>
+			)}
+		</Box>
+	);
 };
 
 export default Chat;
