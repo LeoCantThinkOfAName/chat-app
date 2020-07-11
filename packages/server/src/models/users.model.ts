5aea4f0a4666bae296d7fe8cdd1309ea7fe8deaf
leocantthinkofanamae <i7851@hotmail.com>
2020-07-11 16:26:01 +0800

    fix userStatus enum type and add updatedAt

--- packages/server/src/models/users.model.ts
+++ packages/server/src/models/users.model.ts
@@ -1,82 +1,88 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-import { Sequelize, DataTypes } from 'sequelize';
-import { Application } from '../declarations';
-import { UserStatus } from '@chat-app/shared';
+import { Sequelize, DataTypes } from "sequelize";
+import { Application } from "../declarations";
+import { UserStatus } from "@chat-app/shared";
 
-const status: UserStatus[] = [ 'offline', 'offline', 'afk' ];
+const status: UserStatus[] = ["offline", "offline", "afk"];
 
 export default function(app: Application) {
-	const sequelizeClient: Sequelize = app.get('sequelizeClient');
-	const users = sequelizeClient.define(
-		'users',
-		{
-			id: {
-				type: DataTypes.NUMBER,
-				allowNull: false,
-				primaryKey: true,
-				autoIncrement: true,
-				unique: true,
-			},
-			name: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true,
-			},
-			status: {
-				type: DataTypes.ENUM(...status),
-				allowNull: false,
-				defaultValue: 'online',
-			},
-			description: {
-				type: DataTypes.STRING,
-				allowNull: true,
-				defaultValue: null,
-			},
-			thumbnail: {
-				type: DataTypes.STRING,
-				allowNull: true,
-				defaultValue: null,
-			},
-			background: {
-				type: DataTypes.STRING,
-				allowNull: true,
-				defaultValue: null,
-			},
-			githubId: {
-				type: DataTypes.STRING,
-				allowNull: true,
-				defaultValue: null,
-			},
-			facebookId: {
-				type: DataTypes.STRING,
-				allowNull: true,
-				defaultValue: null,
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			created_at: {
-				type: DataTypes.DATE,
-				allowNull: false,
-				defaultValue: new Date(),
-			},
-		},
-		{
-			hooks: {
-				beforeCount(options: any) {
-					options.raw = true;
-				},
-			},
-		}
-	);
+  const sequelizeClient: Sequelize = app.get("sequelizeClient");
+  const users = sequelizeClient.define(
+    "users",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      status: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        values: status as string[],
+        defaultValue: "online"
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      thumbnail: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      background: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      githubId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      facebookId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      }
+    },
+    {
+      hooks: {
+        beforeCount(options: any) {
+          options.raw = true;
+        }
+      }
+    }
+  );
 
-	// eslint-disable-next-line no-unused-vars
-	(users as any).associate = function(models: any) {
-		// Define associations here
-		// See http://docs.sequelizejs.com/en/latest/docs/associations/
-	};
+  // eslint-disable-next-line no-unused-vars
+  (users as any).associate = function(models: any) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
 
-	return users;
+  return users;
 }
--- packages/server/src/sequelize.ts
+++ packages/server/src/sequelize.ts
@@ -1,10 +1,10 @@
-import { Sequelize } from 'sequelize';
-import { Application } from './declarations';
+import { Sequelize } from "sequelize";
+import { Application } from "./declarations";
 
-export default function (app: Application) {
-  const connectionString = app.get('postgres');
+export default function(app: Application) {
+  const connectionString = app.get("postgres");
   const sequelize = new Sequelize(connectionString, {
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: false,
     define: {
       freezeTableName: true
@@ -12,21 +12,21 @@ export default function (app: Application) {
   });
   const oldSetup = app.setup;
 
-  app.set('sequelizeClient', sequelize);
+  app.set("sequelizeClient", sequelize);
 
-  app.setup = function (...args) {
+  app.setup = function(...args) {
     const result = oldSetup.apply(this, args);
 
     // Set up data relationships
     const models = sequelize.models;
     Object.keys(models).forEach(name => {
-      if ('associate' in models[name]) {
+      if ("associate" in models[name]) {
         (models[name] as any).associate(models);
       }
     });
 
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync());
+    app.set("sequelizeSync", sequelize.sync());
 
     return result;
   };
