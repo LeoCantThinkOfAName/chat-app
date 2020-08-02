497bdd371d1a7502b8d83aac7b15f8aa77f755ce
leocantthinkofanamae <i7851@hotmail.com>
2020-08-02 14:39:23 +0800

    try to figure out how association works

--- packages/server/src/models/messages.model.ts
+++ packages/server/src/models/messages.model.ts
@@ -1,25 +1,46 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-import { Sequelize, DataTypes } from 'sequelize';
-import { Application } from '../declarations';
+import { Sequelize, DataTypes } from "sequelize";
+import { Application } from "../declarations";
 
-export default function (app: Application) {
-  const sequelizeClient: Sequelize = app.get('sequelizeClient');
-  const messages = sequelizeClient.define('messages', {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    hooks: {
-      beforeCount(options: any) {
-        options.raw = true;
+export default function(app: Application) {
+  const sequelizeClient: Sequelize = app.get("sequelizeClient");
+  const messages = sequelizeClient.define(
+    "messages",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+      },
+      message: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      }
+    },
+    {
+      hooks: {
+        beforeCount(options: any) {
+          options.raw = true;
+        }
       }
     }
-  });
+  );
 
   // eslint-disable-next-line no-unused-vars
-  (messages as any).associate = function (models: any) {
+  (messages as any).associate = function(models: any) {
     // Define associations here
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };
