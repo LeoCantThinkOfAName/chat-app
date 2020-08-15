7a115d7b744ee50cc2011da9b783609af0a0df97
leocantthinkofanamae <i7851@hotmail.com>
2020-08-15 18:50:45 +0800

    fix status display

--- packages/client/src/components/Header/UserAvatar.tsx
+++ packages/client/src/components/Header/UserAvatar.tsx
@@ -19,115 +19,112 @@ import { UserStatusColorScheme } from '../../Theme';
 import StatusAvatar from '../StatusAvatar';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    button: {
-      backgroundColor: theme.palette.common.white,
-      padding: theme.spacing(0.6),
-      minWidth: "auto",
-      marginRight: 0,
-      borderRadius: "50%",
-    },
-  })
+	createStyles({
+		button: {
+			backgroundColor: theme.palette.common.white,
+			padding: '2px',
+			minWidth: 'auto',
+			marginRight: 0,
+			borderRadius: '50%',
+		},
+	})
 );
 
 const UserAvatar = () => {
-  const {t} = useTranslation();
-  const classes = useStyles();
-  const avatarRef = useRef<HTMLButtonElement>(null);
-  const [open, setOpen] = useState(false);
-  const {user, setUser, setToken} = useContext(UserContext);
-  const [fetchData, setRequest] = useRest<User>();
+	const { t } = useTranslation();
+	const classes = useStyles();
+	const avatarRef = useRef<HTMLButtonElement>(null);
+	const [ open, setOpen ] = useState(false);
+	const { user, setUser, setToken } = useContext(UserContext);
+	const [ fetchData, setRequest ] = useRest<User>();
 
-  const handleListKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setOpen(false);
-    }
-  };
+	const handleListKeyDown = (event: React.KeyboardEvent) => {
+		if (event.key === 'Tab') {
+			event.preventDefault();
+			setOpen(false);
+		}
+	};
 
-  const handleClose = (e: React.MouseEvent<EventTarget>) => {
-    if (
-      avatarRef.current &&
-      avatarRef.current.contains(e.target as HTMLElement)
-    ) {
-      return;
-    }
+	const handleClose = (e: React.MouseEvent<EventTarget>) => {
+		if (avatarRef.current && avatarRef.current.contains(e.target as HTMLElement)) {
+			return;
+		}
 
-    setOpen(false);
-  };
+		setOpen(false);
+	};
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
+	const handleClick = () => {
+		setOpen(!open);
+	};
 
-  const handleSelect = (e: React.MouseEvent<EventTarget>, status: UserStatus) => {
-    handleClose(e);
-    setRequest({
-      service: "users",
-      method: "patch",
-      data: { status },
-      id: user.id
-    })
-  }
+	const handleSelect = (e: React.MouseEvent<EventTarget>, status: UserStatus) => {
+		handleClose(e);
+		setRequest({
+			service: 'users',
+			method: 'patch',
+			data: { status },
+			id: user.id,
+		});
+	};
 
-  const handleLogout = () => {
-    app.logout();
-    setUser(initValue.user);
-    setToken("");
-  }
+	const handleLogout = () => {
+		app.logout();
+		setUser(initValue.user);
+		setToken('');
+	};
 
-  useEffect(() => {
-    if(fetchData.data && fetchData.data.status !== user.status) {
-      setUser({
-        ...user, status: fetchData.data.status
-      })
-    }
-  }, [fetchData, setUser, user])
+	useEffect(
+		() => {
+			if (fetchData.data && fetchData.data.status !== user.status) {
+				setUser({
+					...user,
+					status: fetchData.data.status,
+				});
+			}
+		},
+		[ fetchData, setUser, user ]
+	);
 
-  return (
-    <React.Fragment>
-      <Button ref={avatarRef} onClick={handleClick} className={classes.button}>
-        <StatusAvatar name={user.name[0]} status={user.status} />
-      </Button>
-      <Popper
-        open={open}
-        anchorEl={avatarRef.current}
-        role={undefined}
-        transition
-        disablePortal
-      >
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin:
-                placement === "bottom" ? "center top" : "center bottom",
-            }}
-          >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList
-                  autoFocusItem={true}
-                  id="user-status-menu"
-                  onKeyDown={handleListKeyDown}
-                >
-                  {Object.keys(UserStatusColorScheme).map((key: string) => (
-                    <MenuItem key={key} onClick={(e) => handleSelect(e, key as UserStatus)}>
-                      <Box height={15} width={15} bgcolor={UserStatusColorScheme[key as UserStatus]} borderRadius="50%" mr={1}/>{t(`general.user.status.${key}`)}
-                    </MenuItem>
-                  ))}
-                  <MenuItem onClick={handleLogout}>
-                    <Box height={15} width={15} bgcolor="red" borderRadius="50%" mr={1}/>
-                    {t(`general.user.logout`)}
-                  </MenuItem>
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Button ref={avatarRef} onClick={handleClick} className={classes.button}>
+				<StatusAvatar name={user.name[0]} thumbnail={user.thumbnail} status={user.status} />
+			</Button>
+			<Popper open={open} anchorEl={avatarRef.current} role={undefined} transition disablePortal>
+				{({ TransitionProps, placement }) => (
+					<Grow
+						{...TransitionProps}
+						style={{
+							transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+						}}
+					>
+						<Paper>
+							<ClickAwayListener onClickAway={handleClose}>
+								<MenuList autoFocusItem={true} id="user-status-menu" onKeyDown={handleListKeyDown}>
+									{Object.keys(UserStatusColorScheme).map((key: string) => (
+										<MenuItem key={key} onClick={(e) => handleSelect(e, key as UserStatus)}>
+											<Box
+												height={15}
+												width={15}
+												bgcolor={UserStatusColorScheme[key as UserStatus]}
+												borderRadius="50%"
+												mr={1}
+											/>
+											{t(`general.user.status.${key}`)}
+										</MenuItem>
+									))}
+									<MenuItem onClick={handleLogout}>
+										<Box height={15} width={15} bgcolor="red" borderRadius="50%" mr={1} />
+										{t(`general.user.logout`)}
+									</MenuItem>
+								</MenuList>
+							</ClickAwayListener>
+						</Paper>
+					</Grow>
+				)}
+			</Popper>
+		</React.Fragment>
+	);
 };
 
 export default UserAvatar;
--- packages/client/src/components/StatusAvatar/index.tsx
+++ packages/client/src/components/StatusAvatar/index.tsx
@@ -1,67 +1,65 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import clsx from "clsx";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Avatar from '@material-ui/core/Avatar';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import React from 'react';
+import Box from '@material-ui/core/Box';
 
 interface Props {
-  name: string;
-  thumbnail?: string | null;
-  status?: "online" | "offline" | "afk" | null;
-  ref?: React.MutableRefObject<any>;
+	name: string;
+	thumbnail?: string | null;
+	status?: 'online' | 'offline' | 'afk' | null;
+	ref?: React.MutableRefObject<any>;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    avatar: {
-      position: "relative",
-      overflow: "visible",
-    },
-    status: {
-      "&::after": {
-        content: "''",
-        display: "block",
-        position: "absolute",
-        height: "calc(100% + 8px)",
-        width: "calc(100% + 8px)",
-        borderStyle: "solid",
-        borderWidth: 2,
-        borderRadius: "50%",
-      },
-    },
-    online: {
-      "&::after": {
-        borderColor: theme.palette.userStatus.online,
-      },
-    },
-    offline: {
-      "&::after": {
-        borderColor: theme.palette.userStatus.offline,
-      },
-    },
-    afk: {
-      "&::after": {
-        borderColor: theme.palette.userStatus.afk,
-      },
-    },
-  })
+	createStyles({
+		wrapper: {
+			padding: '5px',
+			borderRadius: '50%',
+			position: 'relative',
+		},
+		status: {
+			'&::after': {
+				top: 0,
+				left: 0,
+				content: "''",
+				display: 'block',
+				position: 'absolute',
+				height: '100%',
+				width: '100%',
+				borderStyle: 'solid',
+				borderWidth: 3,
+				borderRadius: '50%',
+			},
+		},
+		online: {
+			'&::after': {
+				borderColor: theme.palette.userStatus.online,
+			},
+		},
+		offline: {
+			'&::after': {
+				borderColor: theme.palette.userStatus.offline,
+			},
+		},
+		afk: {
+			'&::after': {
+				borderColor: theme.palette.userStatus.afk,
+			},
+		},
+	})
 );
 
 const StatusAvatar: React.FC<Props> = ({ name, thumbnail, status }) => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Avatar
-      alt={name}
-      className={clsx(
-        classes.avatar,
-        status && classes.status,
-        status && classes[status]
-      )}
-      src={thumbnail ? thumbnail : undefined}
-    >
-      {name && name[0]}
-    </Avatar>
-  );
+	return (
+		<Box className={clsx(classes.wrapper, status && classes.status, status && classes[status])}>
+			<Avatar alt={name} src={thumbnail ? thumbnail : undefined}>
+				{name && name[0]}
+			</Avatar>
+		</Box>
+	);
 };
 
 export default StatusAvatar;
